74c4c548e14ff872b044c61ec5310e3907071042
ignaarguello <igna.arguello2110@hotmail.com>
2023-01-21 00:23:38 -0300

    Correcion Final

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,3 @@
 let BASE_URL = "https://challenge-nextia-mx-back.up.railway.app";
 
-if (process.env.NODE_ENV === 'production') {
-    BASE_URL = process.env.REACT_APP_URL
-}
-
 module.exports = BASE_URL
